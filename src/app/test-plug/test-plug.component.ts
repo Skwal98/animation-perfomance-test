b5db61efdf7050fb86225afa45a3bc3592578b50
Levkovskiy Alex <levkalex98@gmail.com>
2021-12-17 23:03:12 +0300

    test component

--- src/app/test-plug/test-plug.component.ts
+++ src/app/test-plug/test-plug.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test-plug',
   templateUrl: './test-plug.component.html',
   styleUrls: ['./test-plug.component.scss']
 })
-export class TestPlugComponent implements OnInit {
+export class TestPlugComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  id!: string;
+  constructor(private _cd: ChangeDetectorRef) { 
+  }
+
+  @HostListener('transitionend')
+  onTransitionEnd() {
+    console.log('transitionend');
+  }
+
+  ngAfterViewInit(): void {
+    this.id = makeid(10);
+    this._cd.detectChanges();
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+function makeid(length: number) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
 }
