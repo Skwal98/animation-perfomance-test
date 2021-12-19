e59919c72a0925be9214c2eadc16a82fcc8dfa6c
Levkovskiy Alex <levkalex98@gmail.com>
2021-12-19 16:39:45 +0300

    ready

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ComponentRef, HostListener, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, ComponentRef, HostListener, ViewChild, ViewContainerRef } from '@angular/core';
 import { TestPlugComponent } from '../test-plug/test-plug.component';
 
 @Component({
@@ -6,7 +6,7 @@ import { TestPlugComponent } from '../test-plug/test-plug.component';
   templateUrl: './test-component.component.html',
   styleUrls: ['./test-component.component.scss']
 })
-export class TestComponentComponent implements OnInit, AfterViewInit {
+export class TestComponentComponent implements AfterViewInit {
   BLOCK_HEIGHT = 110;
   SCALE = 1.5;
 
@@ -15,7 +15,6 @@ export class TestComponentComponent implements OnInit, AfterViewInit {
   @ViewChild("containerAnswer", {read: ViewContainerRef}) containerAnswer!: ViewContainerRef;
   @ViewChild("containerNextPreview", {read: ViewContainerRef}) containerNextPreview!: ViewContainerRef;
   
-
   private getElementFromContainer(container: ViewContainerRef, index: number): HTMLElement {
     const containerElement = container.element.nativeElement as HTMLElement;
     const parent = containerElement.parentElement;
@@ -34,8 +33,16 @@ export class TestComponentComponent implements OnInit, AfterViewInit {
     return this.getElementFromContainer(this.containerQuestion, 0);
   }
 
-  private get answerElement(): HTMLElement {
+  private get lastAnswerElement(): HTMLElement {
     return this.getElementFromContainer(this.containerAnswer, 0);
+  }
+
+  private get answerElement(): HTMLElement {
+    return this.getElementFromContainer(this.containerAnswer, this.containerAnswer.length - 1);
+  }
+
+  private get _marginAnswer() : number{
+    return window.innerHeight / 100 * 5;
   }
 
   private get _questionYPosition(): number {
@@ -48,15 +55,25 @@ export class TestComponentComponent implements OnInit, AfterViewInit {
   private get _answerYPosition(): number {
     const container = (this.containerAnswer.element.nativeElement as HTMLElement).parentElement;
     const rect = container!.getBoundingClientRect();
-    const yPoint = rect.top + rect.height / 2 - this.BLOCK_HEIGHT * 1.5 / 2;
+    const scaleDiff = (this.SCALE - 1) / 2 + 1;
+    const yPoint = rect.height - this.BLOCK_HEIGHT * scaleDiff - this._marginAnswer;
     return yPoint;
   }
 
+  private _isLoading = false;
   @HostListener('click')
   onClick() {
+    if (!this._isLoading){
+      this._isLoading = true;
+      this._startAnimation();
+    }
+  }
+
+  private _startAnimation(): void {
     this._animatePreview();
     this._animateQuestion();
     this._animateAnswer();
+    this._animateFadeOutAnswer();
     this._animateNextPreview();
   }
 
@@ -76,11 +93,16 @@ export class TestComponentComponent implements OnInit, AfterViewInit {
     }
   }
 
+  private _animateFadeOutAnswer(){
+    if (this.containerAnswer.length > 1 && this.lastAnswerElement){
+      const rect = this.lastAnswerElement!.getBoundingClientRect();
+      this.lastAnswerElement.style.transform = `translate(0, -${rect.height + rect.top + this._marginAnswer}px)`;
+    }
+  }
+
   private _animateAnswer(){
     if (this.answerElement){
-      const container = (this.containerAnswer.element.nativeElement as HTMLElement).parentElement;
-      const rect = container!.getBoundingClientRect();
-      this.answerElement.style.transform = `translate(0, -${rect.height}px)`;
+      this.answerElement.style.transform = `translate(0, -${this.BLOCK_HEIGHT + this._marginAnswer}px)`;
     }
   }
 
@@ -91,37 +113,56 @@ export class TestComponentComponent implements OnInit, AfterViewInit {
   }
 
   constructor() { }
+
   ngAfterViewInit(): void {
-    this._createPreview();
-    setTimeout(() => {
-      this._animateNextPreview();
-    });
+    this._createInitialElements();
   }
 
-  ngOnInit(): void {
-    
+  private _createTestComponentInContainer(container: ViewContainerRef): ComponentRef<TestPlugComponent>{
+    const viewRef = container.createComponent<TestPlugComponent>(TestPlugComponent);
+    viewRef.hostView.detectChanges();
+    return viewRef;
+  }
+
+  private _createInitialElements(): void {
+    this._createPreview();
+    this._createTestComponentInContainer(this.containerPreview);
+    const questionViewRef = this._createTestComponentInContainer(this.containerQuestion);
+    questionViewRef.location.nativeElement.style.transform = `scale(${this.SCALE})`;
   }
 
   private _createPreview(): void{
-    const previewViewRef = this.containerNextPreview.createComponent<TestPlugComponent>(TestPlugComponent);
-    previewViewRef.hostView.detectChanges();
-    previewViewRef.instance.transitionEnd.subscribe(null, null, () => {
-      this.nextPreviewElement.style.removeProperty('transform');
-      if (this.previewElement){
-        this.previewElement.style.transform = `scale(${this.SCALE})`;
-      }
-      if (this.questionElement)
-      {
-       this.questionElement.style.removeProperty('transform');
-      }
+    const previewViewRef = this._createTestComponentInContainer(this.containerNextPreview);
+    previewViewRef.instance.transitionEnd.subscribe(null, null, () => this._onTransitionEnd());
+  }
 
-      this._clearAnswers();
-      this._createPreview();
-      this._moveQuestionToAnswer();
-      this._movePreviewToQuestion();
-      this._moveNextPreviewToPreview();
-    });
+  private _safeSetTransform(element: HTMLElement, value: string | null): void {
+    if (element){
+      if (value){
+        element.style.transform = value;
+      } else {
+        const tempTransition = element.style.transition;
+        element.style.transition = '0s';
+        element.style.removeProperty('transform');
+        setTimeout(() => {
+          element.style.transition = tempTransition;
+        });
+      }
+    }
+  }
 
+  private _onTransitionEnd(): void {
+    this._safeSetTransform(this.nextPreviewElement, null);
+    this._safeSetTransform(this.questionElement, null);
+    this._safeSetTransform(this.answerElement, null);
+    this._safeSetTransform(this.previewElement, `scale(${this.SCALE})`);
+
+    this._clearAnswers();
+    this._createPreview();
+    this._moveQuestionToAnswer();
+    this._movePreviewToQuestion();
+    this._moveNextPreviewToPreview();
+    this._isLoading = false;
   }
 
   private _moveNextPreviewToPreview(): void {
@@ -144,12 +185,14 @@ export class TestComponentComponent implements OnInit, AfterViewInit {
     const viewRef = this.containerQuestion.get(0);
     viewRef?.detach();
     if (viewRef){
-      this.containerAnswer.insert(viewRef, 0);
+      this.containerAnswer.insert(viewRef, this.containerAnswer.length);
     }
   }
 
   private _clearAnswers(): void{
-    const viewRef = this.containerAnswer.get(0);
-    viewRef?.destroy();
+    if (this.containerAnswer.length > 1){
+      const viewRef = this.containerAnswer.get(0);
+      viewRef?.destroy();
+    }
   }
 }
